31283ee9bb565222933523666838a1c2c2c427b5
Suresh Choudhary <sureshchoudhary8636@gmail.com>
2023-12-30 16:45:23 +0530

    Update API Response

--- src/Store/productSlice.js
+++ src/Store/productSlice.js
@@ -55,7 +55,7 @@ export const fetchProducts = () => {
         try{
             const res = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
             const jsonData = await res.json();
-            dispatch(setProducts(jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants))
+            dispatch(setProducts(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants))
             dispatch(setStatus(STATUSES.IDLE));
             // console.log(jsonData);
         }catch(err){
